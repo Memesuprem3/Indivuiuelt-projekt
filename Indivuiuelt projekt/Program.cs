2ebf78d6451bc6f14766e094aece3872a0f2d680
Christian Rapp <christian.rapp@edu.varberg.se>
2023-10-09 10:45:44 +0200

    skapat arrayer för användare samt pin och konton

--- Indivuiuelt projekt/Program.cs
+++ Indivuiuelt projekt/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace Indivuiuelt_projekt
+    
 {
     internal class Program
     {
-        static List<string> användare = new List<string> { }
-
-
+        static List<string> användare = new List<string> { "Anas", "Tobias", "Johanna", "Chris", "Ubbe" };
+        static List<string> pinkod = new List<string> { "1234", "5678", "8912", "3456", "1337" };
+        static List<double[]> Konton = new List<double[]>
+{
+    new double[] {100.00, 500.30},
+    new double[] {1500.00, 700},
+    new double[] {400.00, 10000, 20},
+    new double[] {460.00, 210},
+    new double[] {350, 150}
+};
         static void Main(string[] args)
         {
             Console.WriteLine("Välkomen till Eazy-Bank!");
