0840c85430dc1fa5b47193d9135aa4b18ebb22ec
Christian Rapp <christian.rapp@edu.varberg.se>
2023-10-09 11:15:32 +0200

    Kopplat ihop listor med loggin funktioner

--- Indivuiuelt projekt/Program.cs
+++ Indivuiuelt projekt/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Indivuiuelt_projekt
+﻿using System.Net.NetworkInformation;
+using System.Xml;
+
+namespace Indivuiuelt_projekt
     
 {
     internal class Program
     {
-        static List<string> användare = new List<string> { "Anas", "Tobias", "Johanna", "Chris", "Ubbe" };
-        static List<string> pinkod = new List<string> { "1234", "5678", "8912", "3456", "1337" };
+        static List<string> users = new List<string> { "Anas", "Tobias", "Johanna", "Chris", "Ubbe" };
+        static List<string> pins = new List<string> { "1234", "5678", "8912", "3456", "1337" };
         static List<double[]> Konton = new List<double[]>
 {
     new double[] {100.00, 500.30},
@@ -13,19 +16,56 @@
     new double[] {460.00, 210},
     new double[] {350, 150}
 };
+        static int currentUserIndex = -1;
+        static int loginAttempts = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("Välkomen till Eazy-Bank!");
 
-
+            Program.Login();
            
 
 
         }
 
 
+        static bool Login()
+        {
+            int maxLoginAttempts = 3;
+            int loginAttempts = 0;
+
+            while (loginAttempts < maxLoginAttempts)
+            {
+                Console.Write("Ange användarnamn: ");
+                string username = Console.ReadLine();
+                Console.Write("Ange PIN-kod: ");
+                string pin = Console.ReadLine();
+
+                int userIndex = users.IndexOf(username);
+
+                if (userIndex >= 0 && userIndex < pins.Count && pins[userIndex] == pin)
+                {
+                    currentUserIndex = userIndex;
+                    Console.WriteLine($"Inloggningen lyckades för användare {username}.");
+                    return true;
+                }
+                else
+                {
+                    loginAttempts++;
+                    Console.WriteLine("Inloggningen misslyckades. Försök igen.");
+                    if (loginAttempts >= maxLoginAttempts)
+                    {
+                        Console.WriteLine("Du har skrivit in fel pinkod tre gånger. Programmet startas om.");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            return false;
+        }
 
 
 
     }
+
+
 }
