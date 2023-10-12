35325e18e5fad16f7d503247a1110c959bf70615
Christian Rapp <christian.rapp@edu.varberg.se>
2023-10-12 11:44:55 +0200

    Skapat funktion för överföring av pengar mellan konton.

--- Indivuiuelt projekt/Program.cs
+++ Indivuiuelt projekt/Program.cs
@@ -2,7 +2,7 @@
 using System.Xml;
 
 namespace Indivuiuelt_projekt
-    
+
 {
     internal class Program
     {
@@ -27,7 +27,7 @@ namespace Indivuiuelt_projekt
                 {
                     while (true)
                     {
-                        Console.WriteLine("Välkomen till ditt konto.");
+                        
                         Console.WriteLine("\nVälj vad du vill göra");
                         Console.WriteLine("1. Se över konton & saldo");
                         Console.WriteLine("2. Överföring mellan konton");
@@ -41,9 +41,10 @@ namespace Indivuiuelt_projekt
                                 case 1:
                                     ShowAccountsAndBalnace();
                                     break;
+                                    
                                 case 2:
-                                    //TransferBetweenAccounts();
-                                    break;
+                                    TransferBetweenAccounts();                                   
+                                    break;                                    
                                 case 3:
                                     //WithdrawMoney();
                                     break;
@@ -51,6 +52,7 @@ namespace Indivuiuelt_projekt
                                     //LOgout();
                                     break;
                                 default:
+                                    Console.Clear();
                                     Console.WriteLine("Ogiltigt val.");
                                     break;
                                     {
@@ -76,7 +78,7 @@ namespace Indivuiuelt_projekt
                 string username = Console.ReadLine();
                 Console.Write("Ange PIN-kod: ");
                 string pin = Console.ReadLine();
-
+                Console.Clear();
                 int userIndex = users.IndexOf(username);
 
                 if (userIndex >= 0 && userIndex < pins.Count && pins[userIndex] == pin)
@@ -101,15 +103,54 @@ namespace Indivuiuelt_projekt
 
         static void ShowAccountsAndBalnace()
         {
+            Console.Clear();
             Console.WriteLine("Dina konton och saldon: ");
             double[] userAccounts = Konton[currentUserIndex];
-            for (int i= 0; i < userAccounts.Length; i++)
+            for (int i = 0; i < userAccounts.Length; i++)
             {
-                Console.WriteLine($"Konto {i +1}: {userAccounts[i]}");
+                Console.WriteLine($"Konto {i + 1}: {userAccounts[i]}");
             }
         }
 
+
+        static void TransferBetweenAccounts()
+        {
+            
+            Console.WriteLine("Överföring mellan konton: ");
+            ShowAccountsAndBalnace();
+            Console.WriteLine("Välj ett konto att ta överföra ifrån");
+            int fromAccount = int.Parse(Console.ReadLine()) - 1;
+            if (fromAccount < 0 || fromAccount >= Konton[currentUserIndex].Length)
+            {
+                Console.WriteLine("Ogiltigt kontoval");
+                return;
+
+            }
+            Console.Write("Välj ett konto att flytta pengarna till: ");
+            int toAccount = int.Parse(Console.ReadLine()) - 1;
+            if (toAccount < 0 || toAccount >= Konton[currentUserIndex].Length)
+            {
+                Console.WriteLine("Ogiltigt kontoval.");
+                return;
+            }
+            Console.WriteLine("Ange summan du vill flytta");
+            double amount = double.Parse(Console.ReadLine());
+            if (amount > 0 && Konton[currentUserIndex][fromAccount] >= amount)
+            {
+                Console.Clear();
+                Konton[currentUserIndex][fromAccount] -= amount;
+                Konton[currentUserIndex][toAccount] += amount;
+                Console.WriteLine($"Överföringen är genomförd. Nytt saldo på konto {fromAccount + 1}: {Konton[currentUserIndex][fromAccount]:C}");
+                Console.WriteLine($"Nytt saldo på konto {toAccount + 1}: {Konton[currentUserIndex][toAccount]:C}");
+
+            }
+            else
+            {
+                Console.WriteLine("Ogiltig summa eller otillräckligt saldo.");
+            }
+
+        }
+
+
     }
-
-
 }
